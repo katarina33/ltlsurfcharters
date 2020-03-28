1ab31bb971e018267261d66761506989308c2ec3
Musadiq Khan <musadiq.khan@gmail.com>
2020-03-28 20:36:18 +0500

    Updated Layout.js
    Layoutjs updated

--- src/components/PortfolioBlock.js
+++ src/components/PortfolioBlock.js
@@ -17,38 +17,38 @@ export default class PortfolioBlock extends React.Component {
     );
     let post_len = _.size(recent_projects);
     return (
-      <section
-        id={_.get(this.props, "section.section_id")}
-        className="portfolio-block block outer"
-      >
-        <div className="inner">
-          <div className="block-header">
-            <h2 className="block-title line-top">
-              {_.get(this.props, "section.title")}
-            </h2>
-            {_.get(this.props, "section.subtitle") && (
-              <p className="block-subtitle">
-                {htmlToReact(_.get(this.props, "section.subtitle"))}
-              </p>
-            )}
-          </div>
-          <div className="block-content">
-            <div
-              className={
-                "portfolio-feed layout-" +
-                _.get(this.props, "section.layout_style")
-              }
-            >
-              {_.map(recent_projects, (post, post_idx) => (
-                <article key={post_idx} className="post project">
-                  {post_idx === post_len - 1 ? (
-                    <Link
-                      to={_.get(this.props, "section.view_all_url")}
-                      className="post-link"
-                    >
-                      {_.get(post, "frontmatter.thumb_img_path") && (
-                        <div className="post-thumbnail">
-                          <LazyLoad>
+      <LazyLoad>
+        <section
+          id={_.get(this.props, "section.section_id")}
+          className="portfolio-block block outer"
+        >
+          <div className="inner">
+            <div className="block-header">
+              <h2 className="block-title line-top">
+                {_.get(this.props, "section.title")}
+              </h2>
+              {_.get(this.props, "section.subtitle") && (
+                <p className="block-subtitle">
+                  {htmlToReact(_.get(this.props, "section.subtitle"))}
+                </p>
+              )}
+            </div>
+            <div className="block-content">
+              <div
+                className={
+                  "portfolio-feed layout-" +
+                  _.get(this.props, "section.layout_style")
+                }
+              >
+                {_.map(recent_projects, (post, post_idx) => (
+                  <article key={post_idx} className="post project">
+                    {post_idx === post_len - 1 ? (
+                      <Link
+                        to={_.get(this.props, "section.view_all_url")}
+                        className="post-link"
+                      >
+                        {_.get(post, "frontmatter.thumb_img_path") && (
+                          <div className="post-thumbnail">
                             <img
                               className="thumbnail"
                               src={safePrefix(
@@ -56,45 +56,45 @@ export default class PortfolioBlock extends React.Component {
                               )}
                               alt={_.get(post, "frontmatter.title")}
                             />
-                          </LazyLoad>
-                        </div>
-                      )}
-                      <span className="view-all">
-                        {_.get(this.props, "section.view_all_text") ||
-                          "View All"}
-                      </span>
-                    </Link>
-                  ) : (
-                    <Link
-                      to={safePrefix(_.get(post, "url"))}
-                      className="post-link"
-                    >
-                      {_.get(post, "frontmatter.thumb_img_path") && (
-                        <div className="post-thumbnail">
-                          <LazyLoad>
-                            <img
-                              className="thumbnail"
-                              src={safePrefix(
-                                _.get(post, "frontmatter.thumb_img_path")
-                              )}
-                              alt={_.get(post, "frontmatter.title")}
-                            />
-                          </LazyLoad>
-                        </div>
-                      )}
-                      <header className="post-header">
-                        <h3 className="post-title">
-                          {_.get(post, "frontmatter.title")}
-                        </h3>
-                      </header>
-                    </Link>
-                  )}
-                </article>
-              ))}
+                          </div>
+                        )}
+                        <span className="view-all">
+                          {_.get(this.props, "section.view_all_text") ||
+                            "View All"}
+                        </span>
+                      </Link>
+                    ) : (
+                      <Link
+                        to={safePrefix(_.get(post, "url"))}
+                        className="post-link"
+                      >
+                        {_.get(post, "frontmatter.thumb_img_path") && (
+                          <div className="post-thumbnail">
+                            <LazyLoad>
+                              <img
+                                className="thumbnail"
+                                src={safePrefix(
+                                  _.get(post, "frontmatter.thumb_img_path")
+                                )}
+                                alt={_.get(post, "frontmatter.title")}
+                              />
+                            </LazyLoad>
+                          </div>
+                        )}
+                        <header className="post-header">
+                          <h3 className="post-title">
+                            {_.get(post, "frontmatter.title")}
+                          </h3>
+                        </header>
+                      </Link>
+                    )}
+                  </article>
+                ))}
+              </div>
             </div>
           </div>
-        </div>
-      </section>
+        </section>
+      </LazyLoad>
     );
   }
 }
